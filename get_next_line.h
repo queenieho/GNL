7a4afc1da8682585be0ca6f250a16856d1f25596
Queenie Ho <qho@e1z2r5p7.42.us.org>
2016-12-10 01:09:22 -0800

    Starting

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qho <qho@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/08 23:40:46 by qho               #+#    #+#             */
+/*   Updated: 2016/12/09 16:55:50 by qho              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <stdlib.h>
+
+# define BUFF_SIZE 32
+
+
+
+
+
+# endif
