7e61cf8846d77823092b09349ba1b839d3312690
Queenie Ho <qho@e1z2r5p28.42.us.org>
2017-01-06 20:05:14 -0800

    Updated gnl, somewhat works, with tons of bugs

--- get_next_line.c
+++ get_next_line.c
@@ -5,18 +5,58 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: qho <qho@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/09 16:50:55 by qho               #+#    #+#             */
-/*   Updated: 2016/12/10 00:55:58 by qho              ###   ########.fr       */
+/*   Created: 2016/12/19 11:33:20 by qho               #+#    #+#             */
+/*   Updated: 2017/01/06 20:00:21 by qho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int get_next_line(const int fd, char **line)
-{
-	char buf[BUFF_SIZE];
+static int	ft_get_line(char **line, char **hold)
+{	
+	char	*tmp;
 
-	while (read(fd, &buf, BUFF_SIZE))
-		ft_putstr(buf);
+	if ((tmp = ft_strchr(*hold, '\n')))
+	{
+		*tmp = '\0';
+		*line = ft_strdup(*hold);
+		tmp++;
+		free(*hold);
+		*hold = ft_strdup(tmp);
+		return (1);
+	}
+	return (0);
+}
+
+int	get_next_line(const int fd, char **line)
+{
+	static char	*hold = NULL;
+	char		*buf;
+	int			ret;
+
+	if (fd == -1)
+		return (-1);
+	if (hold)
+	{
+		if (ft_get_line(line, &hold))
+			return (1);
+	}
+	buf = ft_strnew(BUFF_SIZE);
+	while ((ret = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[ret] = '\0';
+		if (hold)
+			hold = ft_strjoin(hold, buf);
+		else
+			hold = ft_strdup(buf);
+		if (ft_get_line(line, &hold))
+			return (1);
+	}
+	if (ft_strcmp(*line, hold) != 0)
+	{
+		*line = ft_strdup(hold);
+		free(hold);
+		return (0);
+	}
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,25 @@
 /*   By: qho <qho@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 23:40:46 by qho               #+#    #+#             */
-/*   Updated: 2016/12/09 16:55:50 by qho              ###   ########.fr       */
+/*   Updated: 2016/12/20 18:23:28 by qho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <fcntl.h>
 # include <unistd.h>
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <stdlib.h>
 
-# define BUFF_SIZE 32
+# include <stdio.h>
 
+# include "libft/libft.h"
 
+# define BUFF_SIZE 10
 
-
+int get_next_line(const int fd, char **line);
 
 # endif
