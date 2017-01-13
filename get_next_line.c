01fdad7d1fa5b246b8c8ce7f74b248a9b919615a
Queenie Ho <qho@e1z2r5p7.42.us.org>
2017-01-13 13:38:45 -0800

    new changes, filecheker almost passed

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,23 @@
 /*   By: qho <qho@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/19 11:33:20 by qho               #+#    #+#             */
-/*   Updated: 2017/01/06 20:00:21 by qho              ###   ########.fr       */
+/*   Updated: 2017/01/13 13:37:53 by qho              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	ft_get_line(char **line, char **hold)
+static int	ft_get_line(char **line, char **hold, char *buf)
 {	
 	char	*tmp;
 
+	if (buf)
+	{
+		if (*hold)
+			*hold = ft_strjoin(*hold, buf);
+		else
+			*hold = ft_strdup(buf);
+	}
 	if ((tmp = ft_strchr(*hold, '\n')))
 	{
 		*tmp = '\0';
@@ -34,29 +41,25 @@ int	get_next_line(const int fd, char **line)
 	char		*buf;
 	int			ret;
 
-	if (fd == -1)
+	if (fd < 0)
 		return (-1);
 	if (hold)
 	{
-		if (ft_get_line(line, &hold))
+		if (ft_get_line(line, &hold, NULL))
 			return (1);
 	}
 	buf = ft_strnew(BUFF_SIZE);
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
-		if (hold)
-			hold = ft_strjoin(hold, buf);
-		else
-			hold = ft_strdup(buf);
-		if (ft_get_line(line, &hold))
+		if (ft_get_line(line, &hold, buf))
 			return (1);
 	}
-	if (ft_strcmp(*line, hold) != 0)
+	if (*hold != '\0')
 	{
 		*line = ft_strdup(hold);
 		free(hold);
-		return (0);
+		return (1);
 	}
 	return (0);
 }
